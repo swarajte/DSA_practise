4d6f0dd437c9e47fb660e8c153cd7b23cb2e6510
Swaraj Telkhade <113779048+swarajte@users.noreply.github.com>
2023-08-06 09:37:02 +0530

    Add files via upload

--- Dynamic Programming/dp on strings/longest_common_substring.cpp
+++ Dynamic Programming/dp on strings/longest_common_substring.cpp
@@ -35,8 +35,55 @@ int longestCommonSubstr(string S1, string S2, int n, int m)
     return maxi;
 }
 
+
+string print_LCSubstr(string X, string Y){
+    int m = X.length();
+    int n = Y.length();
+
+        // tabulation
+    // vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    int len[2][n+1];
+    int currRow=0;
+    int result=0;
+    int end;
+
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if(i==0 || j==0){
+                len[currRow][j] = 0;
+            }
+            else if (X[i - 1] == Y[j - 1])
+            {
+                len[currRow][j] = len[1 - currRow][j - 1] + 1;
+                if (len[currRow][j] > result) {
+                    result = len[currRow][j];
+                    end = i - 1;
+                }
+            }
+            else
+            {
+                len[currRow][j]=0;
+            }
+        }
+        currRow=1-currRow;
+
+    }
+
+    
+
+    if(result==0){
+        return "-1";
+    }
+
+    return X.substr(end-result+1,result);
+
+}
+
 int main()
 {
+    cout<<print_LCSubstr("leetco","leet");
 
     return 0;
 }
